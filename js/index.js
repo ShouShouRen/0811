685178e1d50d2fbaaa169ca31903350429563af4
Peter Chen <peterchen2017125@gmail.com>
2022-08-15 14:58:49 +0800

    20220815

--- js/index.js
+++ js/index.js
@@ -19,6 +19,62 @@ new Chart("myChart", {
     }
 });
 
-function alertSubmit(){
+function alertSubmit() {
     alert('資料已送出感謝您的諮詢');
 }
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).width() < 768) {
+            $("#navbarToggler a").click(function () {
+                $("#navbarToggler").collapse('hide');
+            });
+            $(window).scroll(function () {
+                $("#navbarToggler").collapse('hide');
+            });
+        }
+    })
+    $(".add").each(function (i) {
+        $(this).click(function () {
+            var total = $(".num").eq(i).val();
+            total++;
+            $(".num").eq(i).val(total);
+        });
+    });
+    $(".min").each(function (index) {
+        $(this).click(function () {
+            var total = $(".num").eq(index).val();
+            var n = parseInt(total) - 1;
+            if (n >= 1) {
+                $(".num").eq(index).val(n);
+            } else {
+                $(".num").eq(index).val(0);
+            }
+        });
+    });
+
+    const datan = [0, 0, 0, 0, 0, 0];
+    var allnum = 0;
+    $(".addToCart").click(function () {
+        allnum = 0;
+        for (n = 0; n < 6; n++) {
+            var t = $(".num").eq(n).val();
+            $(".disnum").eq(n).html(t);
+            allnum += parseInt(t);
+            datan[n] = parseInt(t);
+        }
+    })
+
+    $(".submit").click(function () {
+        $(".num").val(0);
+        $(".disnum").val(0);
+    })
+});
+
+$("#gotop").click(function () {
+    $("html,body").animate({
+        scrollTop: 0
+    })
+    return false;
+})
